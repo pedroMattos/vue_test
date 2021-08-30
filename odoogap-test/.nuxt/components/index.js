44ad9e45a9364e5f62d0ee3dd894ed5a026e8178
Pedro <laranjeiraspedro@gmail.com>
2021-08-30 08:35:50 -0300

    [add] nutx configs

---
+++ odoogap-test/.nuxt/components/index.js
@@ -0,0 +1,34 @@
+export { default as MyCart } from '../..\\components\\myCart\\index.vue'
+export { default as NavBar } from '../..\\components\\NavBar\\index.vue'
+export { default as ProductComponent } from '../..\\components\\ProductComponent\\index.vue'
+
+// nuxt/nuxt.js#8607
+function wrapFunctional(options) {
+  if (!options || !options.functional) {
+    return options
+  }
+
+  const propKeys = Array.isArray(options.props) ? options.props : Object.keys(options.props || {})
+
+  return {
+    render(h) {
+      const attrs = {}
+      const props = {}
+
+      for (const key in this.$attrs) {
+        if (propKeys.includes(key)) {
+          props[key] = this.$attrs[key]
+        } else {
+          attrs[key] = this.$attrs[key]
+        }
+      }
+
+      return h(options, {
+        on: this.$listeners,
+        attrs,
+        props,
+        scopedSlots: this.$scopedSlots,
+      }, this.$slots.default)
+    }
+  }
+}
