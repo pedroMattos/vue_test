b90f5d72cd527f112b8a166689d44ab3287014b3
Pedro <laranjeiraspedro@gmail.com>
2021-08-29 23:54:23 -0300

    [add] dynamic page and strucure

--- pages/customProduct/_.vue
+++ pages/customProduct/_.vue
@@ -1,27 +1,39 @@
 <template>
-  <div>
-   {{ product }} 
-  </div>
+  <section>
+    <nav-bar />
+    <div v-if="update" class="banner">
+      <img :src="product[0].img" :alt="product[0].title">
+
+      <div class="infos">
+        <h1>{{ product[0].title }}</h1>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
-import {SfProductCard} from '@storefront-ui/vue'
+import NavBar from '../../components/NavBar'
 import axios from 'axios'
 
 export default {
   components: {
-    SfProductCard,
+    NavBar
   },
   data() {
     return {
-      product: null
+      product: null,
+      update: false
     }
   },
   methods: {
     getProducts () {
       axios.get('http://localhost:3333/products').then((products) => {
         this.product = products.data.filter((el) => {
-          if (el._id == this.$route.params.pathMatch) return el
+          if (el._id == this.$route.params.pathMatch) {
+            document.title = `My store - ${el.title}`
+            this.update = true
+            return el
+          }
         })
       })
     },
